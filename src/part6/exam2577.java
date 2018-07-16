28ed487561deda872e7334f4c2bf657c005ccbd3
Danpatpang <puri8467@gmail.com>
2018-07-16 20:50:13 +0900

    Solve problem until exam8958

---
+++ src/part6/exam2577.java
@@ -0,0 +1,27 @@
+package part6;
+
+import java.util.Scanner;
+
+public class exam2577 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int y = s.nextInt();
+        int z = s.nextInt();
+        long value = (long)x*y*z;
+        String str = Long.toString(value);
+
+        int[] count = new int[10];
+        for (int i = 0; i < count.length; i++) {
+            count[i] = 0;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            int j = Integer.parseInt(str.split("")[i]);
+            count[j]++;
+        }
+        for (int i = 0; i < count.length; i++) {
+            System.out.println(count[i]);
+        }
+    }
+}
---
+++ src/part6/exam2920.java
@@ -0,0 +1,18 @@
+package part6;
+
+import java.util.Scanner;
+
+public class exam2920 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        int[] value = new int[8];
+        boolean[] order = new boolean[3];
+        for (int i = 0; i < 8; i++) {
+            value[i] = Integer.parseInt(str.split(" ")[i]);
+        }
+        for (int i = 0; i < 7; i++) {
+            if(va)
+        }
+    }
+}
