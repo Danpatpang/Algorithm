dfcf714a97e4bbe583272c29c7844ba3fe571965
Danpatpang <puri8467@gmail.com>
2018-07-17 19:07:57 +0900

    Solve problem part6

--- src/part6/exam2920.java
+++ src/part6/exam2920.java
@@ -7,12 +7,29 @@ public class exam2920 {
         Scanner s = new Scanner(System.in);
         String str = s.nextLine();
         int[] value = new int[8];
-        boolean[] order = new boolean[3];
+        boolean[] order = new boolean[2];
+        for (int i = 0; i < 2; i++) {
+            order[i] = true;
+        }
+
         for (int i = 0; i < 8; i++) {
             value[i] = Integer.parseInt(str.split(" ")[i]);
         }
+
         for (int i = 0; i < 7; i++) {
-            if(va)
+            if (value[i] > value[i + 1]) {
+                order[0] = false;
+            } else {
+                order[1] = false;
+            }
+        }
+
+        if (!(order[0] || order[1])) {
+            System.out.println("mixed");
+        } else if (order[0]) {
+            System.out.println("ascending");
+        } else {
+            System.out.println("descending");
         }
     }
 }
