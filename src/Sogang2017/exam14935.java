c703ebb84140407e6f74c840d037346236b3e975
danpatpang <puri8467@gmail.com>
2018-11-08 18:17:03 +0900

    Revert "Revert "Solve Sogan Univ 2017""
    This reverts commit ed950eec89b120bc1cebf08f39caa3f97b70ee2d.

---
+++ src/Sogang2017/exam14935.java
@@ -0,0 +1,29 @@
+package Sogang2017;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class exam14935 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("FA");
+        bw.flush();
+        bw.close();
+    }
+
+//    public static void f(int x) {
+//        if (x == 0) {
+//            System.out.println("FA");
+//            return;
+//        }
+//        int length = (int) Math.log10(x) + 1;
+//        int first = x / (int) Math.pow(10, length - 1);
+//        int result = (length) * first;
+//        if (x == result) {
+//            System.out.println("FA");
+//        } else {
+//            f(result);
+//        }
+//    }
+}
