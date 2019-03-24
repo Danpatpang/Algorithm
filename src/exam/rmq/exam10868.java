ca14ef4f44ccd1149ef5b63fe4ff5a3e70b3ed8d
danpatpang <puri8467@gmail.com>
2019-03-24 18:56:27 +0900

    Solve problem exam10868

--- src/exam/rmq/exam10868.java
+++ src/exam/rmq/exam10868.java
@@ -3,11 +3,12 @@ package exam.rmq;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class exam10868 {
-    public static int[] map;
     public static int[] tree;
+    public static int INF = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,25 +17,52 @@ public class exam10868 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        map = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            map[i] = Integer.parseInt(br.readLine());
+        int size = 1;
+        while (size < N) {
+            size <<= 1;
         }
-        tree = new int[N + 1];
-        init(1, 1, 10);
+
+        tree = new int[size * 2];
+        Arrays.fill(tree, INF);
+
+        for (int i = size; i < size + N; i++) {
+            tree[i] = Integer.parseInt(br.readLine());
+        }
+
+        init();
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int finish = Integer.parseInt(st.nextToken());
+
+            System.out.println(Minimum(size + start - 1, size + finish - 1));
+        }
     }
 
-    public static int init(int node, int start, int end) {
-        if (start == end) {
-            return map[start];
-        } else {
-            return tree[node] = Math.min(init(node * 2, start, (start + end) / 2),
-                    init(node * 2 + 1, (start + end) / 2 + 1, end));
-
+    public static void init() {
+        for (int i = tree.length - 1; i >= 0; i -= 2) {
+            tree[i / 2] = Math.min(tree[i], tree[i - 1]);
         }
     }
 
-    public static int Minimum() {
-        return 0;
+    public static int Minimum(int start, int finish) {
+        int min = INF;
+
+        while (start <= finish) {
+            if (start % 2 == 1) {
+                min = Math.min(min, tree[start]);
+                start++;
+            }
+            if (finish % 2 == 0) {
+                min = Math.min(min, tree[finish]);
+                finish--;
+            }
+
+            start /= 2;
+            finish /= 2;
+        }
+        return min;
     }
 }
