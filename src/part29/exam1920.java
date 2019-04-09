35f722e767d8db88c7a7e43572e23ca2b746da41
Danpatpang <puri8467@gmail.com>
2019-04-09 16:38:48 +0900

    Solve problem exam1920

---
+++ src/part29/exam1920.java
@@ -0,0 +1,59 @@
+package part29;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class exam1920 {
+  public static int[] value;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    int N = Integer.parseInt(br.readLine());
+    value = new int[N];
+
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      value[i] = Integer.parseInt(st.nextToken());
+    }
+    Arrays.sort(value);
+
+    int M = Integer.parseInt(br.readLine());
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < M; i++) {
+      int goal = Integer.parseInt(st.nextToken());
+      boolean isExist = find(0, N - 1, goal);
+
+      if (isExist) {
+        System.out.println(1);
+      } else {
+        System.out.println(0);
+      }
+
+      //      if(Arrays.binarySearch(value, goal) >= 0) {
+      //        System.out.println(1);
+      //      } else {
+      //        System.out.println(0);
+      //      }
+    }
+  }
+
+  public static boolean find(int start, int end, int goal) {
+    while (start <= end) {
+      int temp = (start + end) / 2;
+      int result = Integer.compare(value[temp], goal);
+
+      if (result == 0) {
+        return true;
+      } else if (result < 0) {
+        start = temp + 1;
+      } else {
+        end = temp - 1;
+      }
+    }
+    return false;
+  }
+}
