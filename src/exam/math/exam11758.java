a66f6a4b7944085c46659633578014e9a20dcb06
Danpatpang <puri8467@gmail.com>
2019-04-17 17:48:21 +0900

    Revert "Solve problem exam2042(BIT)"
    This reverts commit 786b943c6b31028aef22e44153ed14734f892d17.

--- src/exam/math/exam11758.java
+++ src/exam/math/exam11758.java
@@ -6,26 +6,26 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class exam11758 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
 
-		int[] x = new int[3];
-		int[] y = new int[3];
+    int[] x = new int[3];
+    int[] y = new int[3];
 
-		for (int i = 0; i < 3; i++) {
-			st = new StringTokenizer(br.readLine());
-			x[i] = Integer.parseInt(st.nextToken());
-			y[i] = Integer.parseInt(st.nextToken());
-		}
+    for (int i = 0; i < 3; i++) {
+      st = new StringTokenizer(br.readLine());
+      x[i] = Integer.parseInt(st.nextToken());
+      y[i] = Integer.parseInt(st.nextToken());
+    }
 
-		int temp = (x[1] - x[0]) * (y[2] - y[0]) - (x[2] - x[0]) * (y[1] - y[0]);
-		if (temp > 0) {
-			System.out.println(1);
-		} else if (temp < 0) {
-			System.out.println(-1);
-		} else {
-			System.out.println(0);
-		}
-	}
+    int temp = (x[1] - x[0]) * (y[2] - y[0]) - (x[2] - x[0]) * (y[1] - y[0]);
+    if (temp > 0) {
+      System.out.println(1);
+    } else if (temp < 0) {
+      System.out.println(-1);
+    } else {
+      System.out.println(0);
+    }
+  }
 }
--- src/part36/exam2188.java
+++ src/part36/exam2188.java
@@ -9,16 +9,47 @@ import java.util.StringTokenizer;
 public class exam2188 {
 
   public static boolean[] isVisit;
-  public static int flow[][], backflow[][], N, M;
+  public static int edge[][], N, M, B[];
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
-
-    flow = new int[N + 1][M + 1];
-    backflow = new int[N + 1][N + 1];
+    edge = new int[N + 1][M + 1];
     isVisit = new boolean[N + 1];
+    B = new int[N + 1];
+
+    for (int i = 1; i <= N; i++) {
+      st = new StringTokenizer(br.readLine());
+      int length = Integer.parseInt(st.nextToken());
+      Arrays.fill(edge[i], -1);
+      for (int j = 1; j <= length; j++) {
+        edge[i][j] = Integer.parseInt(st.nextToken());
+      }
     }
+
+    int result = 0;
+    for (int i = 1; i <= N; i++) {
+    	if(DFS(i)) {
+    		result++;
+	    }
+    }
+    System.out.println(result);
+  }
+
+  public static boolean DFS(int start) {
+    isVisit[start] = true;
+    for (int i = 1; i <= M; i++) {
+      if (edge[start][i] == -1) {
+        break;
+      }
+      int v = edge[start][i];
+      if (B[v] == 0 || (!isVisit[B[v]] && DFS(B[v]))) {
+        B[v] = start;
+        return true;
+      }
+    }
+    return false;
+  }
 }
