d39d8ae51acf11c945aa7c2f50b0cf9cd423ac23
Danpatpang <puri8467@gmail.com>
2019-04-20 19:44:50 +0900

    Solve problem exam2188

--- src/part36/exam2188.java
+++ src/part36/exam2188.java
@@ -8,48 +8,74 @@ import java.util.StringTokenizer;
 
 public class exam2188 {
 
-  public static boolean[] isVisit;
-  public static int edge[][], N, M, B[];
+	public static boolean[] isVisit;
+	public static int[] parent;
+	public static int edge[][], N, M;
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    N = Integer.parseInt(st.nextToken());
-    M = Integer.parseInt(st.nextToken());
-    edge = new int[N + 1][M + 1];
-    isVisit = new boolean[N + 1];
-    B = new int[N + 1];
+	public static void back(int n) {
+		while (n != 0) {
+			int child = n;
+			n = parent[child];
+			edge[n][child]--;
+			edge[child][n]++;
+		}
+	}
 
-    for (int i = 1; i <= N; i++) {
-      st = new StringTokenizer(br.readLine());
-      int length = Integer.parseInt(st.nextToken());
-      Arrays.fill(edge[i], -1);
-      for (int j = 1; j <= length; j++) {
-        edge[i][j] = Integer.parseInt(st.nextToken());
-      }
-    }
+	public static boolean DFS(int start) {
+		if (start == N + M + 1) {
+			return true;
+		}
 
-    int result = 0;
-    for (int i = 1; i <= N; i++) {
-    	if(DFS(i)) {
-    		result++;
-	    }
-    }
-    System.out.println(result);
-  }
+		for (int i = 0; i <= N + M + 1; i++) {
+			if (edge[start][i] <= 0 || isVisit[i]) {
+				continue;
+			}
+			parent[i] = start;
+			isVisit[i] = true;
+			if (DFS(i)) {
+				return true;
+			}
+		}
 
-  public static boolean DFS(int start) {
-    isVisit[start] = true;
-    for (int i = 1; i <= M; i++) {
-      if (edge[start][i] == -1) {
-        break;
-      }
-      int v = edge[start][i];
-      if (B[v] == 0 || (!isVisit[B[v]] && DFS(B[v]))) {
-        B[v] = start;
-        return true;
-      }
-    }
-    return false;
-  }
+		return false;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		edge = new int[N + M + 2][N + M + 2];
+		parent = new int[N + M + 2];
+		isVisit = new boolean[N + M + 2];
+
+		// Start
+		for (int i = 1; i <= N; i++) {
+			edge[0][i] = 1;
+		}
+
+		// Finish
+		for (int i = N + 1; i <= N + M; i++) {
+			edge[i][N + M + 1] = 1;
+		}
+
+		// Input
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int length = Integer.parseInt(st.nextToken());
+			for (int j = 1; j <= length; j++) {
+				int node = Integer.parseInt(st.nextToken());
+				edge[i][N + node] = 1;
+			}
+		}
+
+		int result = 0;
+		while (DFS(0)) {
+			result++;
+			back(N + M + 1);
+			Arrays.fill(isVisit, false);
+		}
+
+		System.out.println(result);
+	}
 }
