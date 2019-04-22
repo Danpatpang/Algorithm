3856b0b73ec1f9fb41d6dfd193bd4dab0075a541
Danpatpang <puri8467@gmail.com>
2019-04-22 22:08:08 +0900

    Solve problem exam2902, 10952, 11650

---
+++ src/exam/exam11650.java
@@ -0,0 +1,46 @@
+package exam;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class exam11650 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		Point[] p = new Point[N];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			p[i] = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+		}
+
+		Arrays.sort(p, new Comparator<Point>() {
+			@Override
+			public int compare(Point o1, Point o2) {
+				if (o1.x != o2.x) {
+					return o1.x - o2.x;
+				}
+				return o1.y - o2.y;
+			}
+		});
+
+		for (int i = 0; i < N; i++) {
+			System.out.println(p[i].x + " " + p[i].y);
+		}
+	}
+}
+
+class Point {
+	int x;
+	int y;
+
+	Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
