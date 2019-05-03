f34d986cdcf814cd60f0ae74fd1e3047d1973391
Danpatpang <puri8467@gmail.com>
2019-05-03 22:50:27 +0900

    Solve problem exam1850

---
+++ src/exam/gcd/exam1850.java
@@ -0,0 +1,35 @@
+package exam.gcd;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class exam1850 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < GCD(A, B); i++) {
+			sb.append("1");
+		}
+		System.out.println(sb.toString());
+
+	}
+
+	public static long GCD(long x, long y) {
+		if (x < y) {
+			long temp = x;
+			x = y;
+			y = temp;
+		}
+		if (x % y == 0) {
+			return y;
+		} else {
+			return GCD(y, x % y);
+		}
+	}
+}
