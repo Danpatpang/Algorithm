a893258316733229eb099c820bb1991550047ce1
Danpatpang <puri8467@gmail.com>
2019-05-12 22:21:42 +0900

    Solve problem exam2133(fail)

---
+++ src/dp/exam2133.java
@@ -0,0 +1,25 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class exam2133 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N + 1];
+
+		if (N % 2 == 1) {
+			System.out.println(0);
+		} else {
+			dp[1] = 1;
+			dp[2] = 2;
+			for (int i = 3; i <= N; i++) {
+				dp[i] = dp[i - 1] + dp[i - 2];
+			}
+
+			System.out.println((dp[N] * 2) - 1);
+		}
+	}
+}
