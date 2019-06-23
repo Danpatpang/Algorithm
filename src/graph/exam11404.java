97eadbcc8ff220d31f3c536f732042af33badeeb
Danpatpang <puri8467@gmail.com>
2019-06-23 23:56:15 +0900

    Solve problem exam11404

---
+++ src/graph/exam11404.java
@@ -0,0 +1,53 @@
+package graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class exam11404 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		int[][] map = new int[n + 1][n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (i == j) {
+					continue;
+				}
+				map[i][j] = 1000000000;
+			}
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int value = Integer.parseInt(st.nextToken());
+			map[start][end] = Math.min(map[start][end], value);
+		}
+
+		for (int k = 1; k <= n; k++) {
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= n; j++) {
+					map[i][j] = Math.min(map[i][j], (map[i][k] + map[k][j]));
+				}
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (map[i][j] >= 1000000000) sb.append("0 ");
+				else sb.append(map[i][j] + " ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
