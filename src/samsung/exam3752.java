86509ebdd5112c81aab11e7b5876dc2109926ee5
Danpatpang <puri8467@gmail.com>
2019-08-11 23:56:20 +0900

    Solve problem exam3752

--- src/samsung/exam3752.java
+++ src/samsung/exam3752.java
@@ -7,8 +7,7 @@ import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class exam3752 {
-	static HashSet<Integer> set;
-	static int testNum;
+	static int testNum, sum;
 	static int[] value;
 
 	public static void main(String[] args) throws IOException {
@@ -16,26 +15,31 @@ public class exam3752 {
 		int T = Integer.parseInt(br.readLine());
 		for (int i = 1; i <= T; i++) {
 			StringBuffer sb = new StringBuffer("#" + i + " ");
-			set = new HashSet<Integer>();
 			testNum = Integer.parseInt(br.readLine());
+			boolean[] dp = new boolean[10101];
 			StringTokenizer st = new StringTokenizer(br.readLine());
 
+			dp[0] = true;
+			sum = 0;
 			value = new int[testNum];
 			for (int j = 0; j < testNum; j++) {
 				value[j] = Integer.parseInt(st.nextToken());
+				sum += value[j];
 			}
 
-			DFS(0, 0);
-			sb.append(set.size());
+			for (int j = 0; j < testNum; j++) {
+				for (int k = sum; k >= 0; k--) {
+					if (dp[k]) {
+						dp[value[j] + k] = true;
+					}
+				}
+			}
+
+			int result = 0;
+			for (boolean j : dp) if (j) result++;
+
+			sb.append(result);
 			System.out.println(sb.toString());
 		}
-	}
-
-	private static void DFS(int result, int num) {
-		set.add(result);
-		if (num == testNum) return;
-
-		DFS(result, num + 1);
-		DFS(result + value[num], num + 1);
 	}
 }
