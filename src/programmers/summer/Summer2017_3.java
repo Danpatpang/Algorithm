a40a91610dcc46020b3b6f252a1f261cf733ae4e
Danpatpang <puri8467@gmail.com>
2019-08-21 23:43:33 +0900

    Solve problem Summer2017_3

--- src/programmers/summer/Summer2017_3.java
+++ src/programmers/summer/Summer2017_3.java
@@ -1,31 +1,28 @@
 package programmers.summer;
 
-import java.util.Arrays;
-
 public class Summer2017_3 {
-	private static boolean[] isVisit, isPrime;
-
-	public int solution(int[] nums) {
-		isVisit = new boolean[3000];
-		isPrime = new boolean[3000];
-		Arrays.fill(isPrime, true);
-		isPrime[1] = false;
+	private static boolean[] isNotPrime;
+	private static void makePrime() {
+		isNotPrime = new boolean[3000];
+		isNotPrime[1] = true;
 		for (int i = 2; i <= Math.sqrt(3000); i++) {
-			if (isPrime[i]) {
+			if (!isNotPrime[i]) {
 				for (int j = i + i; j < 3000; j += i) {
-					isPrime[j] = false;
+					isNotPrime[j] = true;
 				}
 			}
 		}
+	}
 
+	public int solution(int[] nums) {
 		int count = 0;
-		for (int i = 0; i < nums.length - 2; i++) {
-			for (int j = i + 1; j < nums.length - 1; j++) {
+		makePrime();
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
 				for (int k = j + 1; k < nums.length; k++) {
 					int temp = nums[i] + nums[j] + nums[k];
-					if (!isVisit[temp] && isPrime[temp]) {
+					if (!isNotPrime[temp]) {
 						count++;
-						isVisit[temp] = true;
 					}
 				}
 			}
